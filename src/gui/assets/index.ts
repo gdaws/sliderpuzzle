72b4c69a22262a063d8f4b5e81d10f684a1c70ca
Graham Daws <graham.daws@gmail.com>
2021-11-11 21:42:15 +0000

    Add two more background pictures

--- src/gui/assets/index.ts
+++ src/gui/assets/index.ts
@@ -1,4 +1,6 @@
 import picture1 from './picture1.jpg';
 import picture2 from './picture2.jpg';
+import picture3 from './picture3.jpg';
+import picture4 from './picture5.jpg';
 
-export const pictures = [picture1, picture2];
+export const pictures = [picture1, picture2, picture3, picture4];
